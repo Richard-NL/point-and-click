46fc3aaa8c90920b1cc7443faa558cd933de3fe4
Richard Hoogstad <rshoogstad@gmail.com>
2015-01-29 22:25:17 +0100

    updated provision.sh

--- provision.sh
+++ provision.sh
@@ -6,3 +6,9 @@ sudo gem install sass
 sudo npm install -g bower
 sudo npm install -g grunt-cli
 sudo npm install --global gulp
+sudo npm install gulp
+sudo npm install gulp-copy
+sudo npm install gulp-concat
+sudo npm install gulp-watch
+sudo npm install gulp-batch
+
